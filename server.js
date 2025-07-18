f2c2da5ba03906f8d9d966873b12744de58ee73f
Nitesh <kumarnitesh2601@gmail.com>
2025-07-18 11:24:16 +0530

    Added /ping route to prevent cold starts

--- server.js
+++ server.js
@@ -27,6 +27,14 @@ app.use(cors());
 app.use('/auth',AuthRouter);
 app.use('/user',UserRouter);
 
+app.get('/', (req, res) => {
+  res.send('Backend is working!');
+});
+
+app.get('/ping', (req, res) => {
+  res.status(200).send('pong');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
